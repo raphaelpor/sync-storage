60a70929e9faf4b4faeeb72ea27b56e16e686a40
Raphael Freitas <raphael.freitas@corp.globo.com>
2017-12-02 16:26:41 -0200

    Add eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 // @flow
-import {
-  AsyncStorage,
-} from 'react-native';
+import { AsyncStorage } from 'react-native';
 
 export function handleError(func) {
   const message = `${func}() requires at least a key as its first parameter.`;
-  console.warn(message);
+  console.warn(message); // eslint-disable-line no-console
   return Promise.reject(message);
 }
 
@@ -14,22 +12,21 @@ class SyncStorage {
   loading: boolean = true;
 
   init(keys: Array<any>): Promise {
-    return AsyncStorage.multiGet(keys)
-      .then((data: Array<Array<string>>): Array<*> => {
-        data.forEach((item: Array<string>) => {
-          this.data.set(item[0], item[1] && JSON.parse(item[1]));
-          this.loading = false;
-        });
-
-        return [...this.data];
+    return AsyncStorage.multiGet(keys).then((data: Array<Array<string>>): Array<*> => {
+      data.forEach((item: Array<string>) => {
+        this.data.set(item[0], item[1] && JSON.parse(item[1]));
+        this.loading = false;
       });
+
+      return [...this.data];
+    });
   }
 
   get(key: any): any {
     return this.data.get(key);
   }
 
-  set(key: any, value: any, dontTryAgain?: boolean): Promise {
+  set(key: any, value: any): Promise {
     if (!key) return handleError('set');
 
     this.data.set(key, value);
