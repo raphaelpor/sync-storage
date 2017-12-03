27400cb7e3539dd73db87ffe2f1c8982ea21bef8
Raphael Freitas <raphael.freitas@corp.globo.com>
2017-12-02 22:21:50 -0200

    Update DevApp example

--- DevApp/App.js
+++ DevApp/App.js
@@ -6,36 +6,12 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+import SyncStorage from './sync-storage';
 
 const styles = StyleSheet.create({
   container: {
@@ -49,9 +25,33 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
+
+class App extends Component<{}> {
+  constructor() {
+    super();
+    this.state = {
+      data: '',
+      loading: true,
+    };
+  }
+
+  async componentWillMount() {
+    const storageKeys = ['module-name'];
+    await SyncStorage.init(storageKeys);
+    const data = SyncStorage.get('module-name');
+    this.setState({ data, loading: false });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          {this.state.loading ? 'Loading...' : `Welcome to ${this.state.data}!`}
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
