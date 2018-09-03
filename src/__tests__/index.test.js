a56c5c77ff384fa89f44a236826254b639fdaa8f
Soorena <s.soorena@gmail.com>
2018-09-03 14:06:44 +0430

    Add test for push update and getAllKeys methods

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -39,3 +39,35 @@ test("Can update an item", () => {
   SyncStorage.saveItem(['bar', 'baz']);
   expect(SyncStorage.get('bar')).toBe('baz');
 });
+
+test('Can update an item with update method', () => {
+  SyncStorage.set('some_key', 'some_value');
+  SyncStorage.update('some_key', 'some_other_value');
+  expect(SyncStorage.get('some_key'))
+    .toBe('some_other_value');
+});
+
+test('Returns a error when update() is called with an undefined key', () => {
+  expect.assertions(1);
+  return SyncStorage.update('undefined_key', 'some_value')
+    .catch((error) => {
+      expect(error)
+        .toMatch('You can not update a key that has not been set.');
+    });
+});
+
+test('Can get all keys from storage', () => {
+  const all_keys = SyncStorage.getAllKeys();
+  SyncStorage.set('foo', 'bar');
+  expect(all_keys)
+    .toEqual(expect.arrayContaining(['foo']));
+});
+
+test('Can push a value to a key with array value', () => {
+  SyncStorage.push('array_key', 'a');
+  expect(SyncStorage.get('array_key'))
+    .toEqual(['a']);
+  SyncStorage.push('array_key', 'b');
+  expect(SyncStorage.get('array_key'))
+    .toEqual(['a', 'b']);
+});
