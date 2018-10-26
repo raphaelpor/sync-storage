2890ec0fe74a78750b70c6c720c8ef158ebd96cd
Soorena Soleimani <s.soorena@gmail.com>
2018-10-26 18:19:38 +0330

    Remove push and update functions

--- src/index.js
+++ src/index.js
@@ -52,32 +52,6 @@ class SyncStorage {
   getAllKeys(): Array<*> {
     return Array.from(this.data.keys());
   }
-
-  update(key: KeyType, value: any): Promise<*> {
-    if (!key) return handleError('update', 'a key');
-
-    const item = this.get(key);
-    if (item !== undefined) {
-      return this.set(key, value);
-    }
-
-    return handleError('You can not update a key that has not been set.');
-  }
-
-  push(key: KeyType, value: any): Promise<*> {
-    if (!key) return handleError('push', 'a key');
-
-    const currentValue = this.get(key);
-    if (currentValue === undefined) {
-      return this.set(key, [value]);
-    }
-
-    if (Array.isArray(currentValue)) {
-      return this.set(key, [...currentValue, value]);
-    }
-
-    return handleError(`Existing Value should be undefined or of type Array, received ${typeof currentValue}`);
-  }
 }
 
 const syncStorage = new SyncStorage();
